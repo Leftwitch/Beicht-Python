028510e775f0578023a7916956b8788bb81f25e0
Justin Guckes <justin.guckes@gmail.com>
2022-02-15 09:09:49 +0100

    Changed loop in Ab3

--- AB2/Aufgabe3.py
+++ AB2/Aufgabe3.py
@@ -13,10 +13,27 @@ def erstelleSpielkonstellationen(player_count):
 
 def erstelleSpielkonstellationen2(player_count):
     result = []
-    for player_index in range(1, player_count+1):
-       # Calculate all possible opponents and append them to result
-        result.extend([[player_index, opponent_index]
-                      for opponent_index in range(player_index, player_count+1) if player_index != opponent_index])
+    current_player = 1
+    current_player_index = 0
+    while current_player_index + 1 < player_count:
+        opponent_index = current_player % player_count
+
+        # Wenn der Spieler bereits gespielt hat, dann nächsten Spieler, bzw von vorne anfangen
+        if opponent_index == 0:
+            current_player_index += 1
+
+        # Wenn wir nicht selbst der Spieler sind, dann Spielkonstellation hinzufügen
+        if current_player_index != opponent_index:
+            # Hier beide möglichkeiten erzeugen
+            element1 = [current_player_index+1, opponent_index+1],
+            element2 = [opponent_index+1, current_player_index+1]
+
+            # Wenn die Konstellation noch nicht vorhanden ist, dann hinzufügen
+            if element1 not in result and element2 not in result:
+                result.append([current_player_index+1, opponent_index+1])
+
+        # Zähler für den nächsten Spieler erhöhen
+        current_player += 1
 
     return result
 
