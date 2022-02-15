3026950c6436285d3d819416fa63dba5c371ed74
Justin Guckes <justin.guckes@gmail.com>
2022-02-15 12:17:39 +0100

    Improve AB3/1

--- AB2/Aufgabe3.py
+++ AB2/Aufgabe3.py
@@ -1,13 +1,16 @@
-hardcoded = [[1, 2], [1, 3], [1, 4], [1, 5], [
-    2, 3], [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+hardcoded = [
+    [1, 2], [1, 3], [1, 4], [1, 5],
+    [2, 3], [2, 4], [2, 5],
+    [3, 4], [3, 5],
+    [4, 5]
+]
 
 
 def erstelleSpielkonstellationen(player_count):
     result = []
     for player_index in range(1, player_count+1):
-        for opponent_index in range(player_index, player_count+1):
-            if player_index != opponent_index:
-                result.append([player_index, opponent_index])
+        for opponent_index in range(player_index+1, player_count+1):
+            result.append([player_index, opponent_index])
     return result
 
 
